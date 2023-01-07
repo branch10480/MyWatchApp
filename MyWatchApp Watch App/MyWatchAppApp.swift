518bc952197c546f0ac5e6b1ee4aa706ab2780b2
branch10480 <toshiharu.imaeda@gmail.com>
2023-01-07 15:56:05 +0900

    Initial Commit

---
+++ MyWatchApp Watch App/MyWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWatchAppApp.swift
+//  MyWatchApp Watch App
+//
+//  Created by Toshiharu Imaeda on 2023/01/07.
+//
+
+import SwiftUI
+
+@main
+struct MyWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
